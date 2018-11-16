f57ccf8d9b123a86c1eab7b449e286f768253ca2
Michael Daniloff <michael.daniloff@gmail.com>
2018-11-16 16:28:54 -0500

    update

--- WiredBrainCoffee.AdminApp/Settings/AppSettings.cs
+++ WiredBrainCoffee.AdminApp/Settings/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public class AppSettings
 	{
-		public static readonly string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=wiredbraincafe;AccountKey=FHk/7dwfG2urNRGJQcQjjBGTpDY/L6WcuPQsNR7ZfYiG9V7lUeWZZ3pLCngB/RPyUrAsLBcbNW6UXzr6rQmG9g==;EndpointSuffix=core.windows.net";
+		public static readonly string ConnectionString = "";
 
 		// Emulator
 		//= "UseDevelopmentStorage=true";
